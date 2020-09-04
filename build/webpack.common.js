feb0446d2325ed2b2e25c5586bf1b5da2d305cc9
liurongqing <phpgege@gmail.com>
2020-09-05 00:51:47 +0800

    优化打包操作

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -1,5 +1,7 @@
 const path = require('path')
+const webpack = require('webpack')
 const { resolve } = path
+
 const config = {
   entry: './src/main.ts',
   output: {
@@ -7,8 +9,18 @@ const config = {
     filename: 'game.js'
   },
   module: {
-    rules: [{ test: /\.ts$/, use: 'babel-loader' }]
+    rules: [{
+      test: /\.ts$/,
+      use: 'babel-loader',
+      exclude: /node_modules/,
+    }]
   },
+  plugins: [
+    new webpack.IgnorePlugin({
+      resourceRegExp: /phaser/,
+      contextRegExp: /phaser/
+    })
+  ],
   resolve: {
     extensions: ['.ts', '.js'],
     alias: {
--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -3,6 +3,8 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const common = require('./webpack.common.js')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 
 module.exports = merge(common, {
   mode: 'production',
